32faf9585b985c154cfd6b813cf1ffa86d80bba9
Poo <poohoka@gmail.com>
2024-05-18 14:31:05 +0700

    - [enh]: startup script

--- docker/startup-dev.sh
+++ docker/startup-dev.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# this enables dependencies reset during development without image rebuild
+if [ ! -d "node_modules" ]; then
+  rm -rf yarn.lock package-lock.json
+  yarn install
+fi
+
 # always migrate during startup
 yarn run migrate
 # run command
