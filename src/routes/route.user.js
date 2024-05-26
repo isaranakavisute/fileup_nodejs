f81b0befa0c6a97c1578988097e5b7da684c976b
Isara Nakavisute <isara_nakavisute@hotmail.com>
2024-05-26 07:32:32 +0700

    set upload file path

--- src/routes/route.user.js
+++ src/routes/route.user.js
@@ -15,20 +15,16 @@ const userRoute = (app) => {
       });
 
     app.get('/fileupload', (req, res) => {
-
-    res.send(__dirname)
-
-    //   var form = new formidable.IncomingForm();
-    //   form.parse(req, function (err, fields, files) {
-    //     var oldpath = files.filetoupload.filepath;
-    //     var newpath = __dirname + files.filetoupload.originalFilename;
-    //     fs.rename(oldpath, newpath, function (err) {
-    //       if (err) throw err;
-    //       res.write('File uploaded and moved!');
-    //       res.end();
-    //     });
-    //   });
-    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.filepath;
+        var newpath = '/root/Backend/uploaded_files/' + files.filetoupload.originalFilename;
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded');
+          res.end();
+        });
+      });
    });
 
     app.get("/me", {
